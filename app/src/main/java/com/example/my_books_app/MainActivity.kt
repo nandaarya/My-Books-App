47523d5c3750d8e24be7bcf08de08fbaaeca0c37
Nanda Arya Putra <nandaarya404@gmail.com>
2023-08-25 22:06:44 +0700

    add recyclerView

--- app/src/main/java/com/example/my_books_app/MainActivity.kt
+++ app/src/main/java/com/example/my_books_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.my_books_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.my_books_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
